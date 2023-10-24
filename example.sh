3151ca7ecd6bf42c325b20a2579bd86b8ad59673
Roger Huba <rogerhuba@gmail.com>
2023-10-24 16:35:41 -0500

    add demo

---
+++ example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script:   Name of the Script?
+# Author:   Who is the Author?
+# Date:     Date or Revisions?
+# Purpose:  What does it do?
+# How:      How do you execute it?
+# Variables: Main Varaibles Used
+# Libraries: What libraries Uses
+# Externals Sources:
+
+# Ask the User how old they are. 
+
+# User: How old are you. The user says.  I'm 49. You store this in a variable
+# called user_age
+
+# user_age = 49
+# print(user_age) -> 49
+
+echo "hi Roger"
+echo "bye"
